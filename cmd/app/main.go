d2988306f08ea04123f54d12f219295a42632828
gucchi0421 <gucchi.1999.1121.0421@gmail.com>
2025-01-07 23:43:10 +0900

    docs: Taskfileの修正

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+type Name struct {
+	First string `json:"first"`
+	Last  string `json:"last"`
+}
+
+func (m *Name) String() string {
+	return fmt.Sprintf("%s %s", m.First, m.Last)
+}
 
 func main() {
-	fmt.Println("Hello World")
+	t := time.Now().Format(time.RFC3339)
+	my := &Name{First: "John", Last: "Doe"}
+	fmt.Printf("Hello! %v the time is %v", my.String(), t)
 }
