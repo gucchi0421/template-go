b5ae9d1ee63fe09c737cb081b23af4ef98e1a364
gucchi0421 <gucchi.1999.1121.0421@gmail.com>
2025-01-12 00:56:04 +0900

    Removes docker-compose and refactors main function
    docker-composeファイルを削除し、デプロイのシンプル化を図る。
    main関数をリファクタリングし、余分なコードを削除、シンプルな出力を実現する。

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,21 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
-
-type Name struct {
-	First string `json:"first"`
-	Last  string `json:"last"`
-}
-
-func (m *Name) String() string {
-	return fmt.Sprintf("%s %s", m.First, m.Last)
-}
+import "fmt"
 
 func main() {
-	t := time.Now().Format(time.RFC3339)
-	my := &Name{First: "John", Last: "Doe"}
-	fmt.Printf("Hello! %v the time is %v", my.String(), t)
+	fmt.Println("Hello, World!")
 }
